a6ab592c197681cd27c0247b3de8c59a1b567fb9
X-White-Veg <2765024335@qq.com>
2020-11-19 21:48:07 +0800

    python基础

---
+++ 01技术学习/day02/code/ex01.py
@@ -0,0 +1,11 @@
+# 显示指令
+# encoding:utf-8
+#import ex03
+import sys
+from sys import path
+sys.path.append("..")
+
+from model01.ex02 import test, test1
+
+test.ppp()
+test1()
---
+++ 01技术学习/day02/code/model01/ex02.py
@@ -0,0 +1,7 @@
+
+class test:
+    def ppp():
+        print("123")
+
+def test1():
+    print("123")
